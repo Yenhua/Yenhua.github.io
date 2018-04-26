84e8afce66f9c02b8298e837d9822aba6f7e5119
Yenhua <Yenhua@users.noreply.github.com>
2018-04-26 23:56:52 +1200

    Updated 10ICT homepage
    There's links for lesson 1

---
+++ Week1Demos/scripts/footToCm.js
@@ -0,0 +1,8 @@
+function footToCm(){
+	var foot = 4; //this is a number and variable declaration
+	var cm = foot*30.48; //this is a assignment statement
+	
+	var result = "We converted 4 foot to " + cm + "cm";//this is a string
+	console.log(result); //this is a built in JS method that ptints to the console
+}
+
